41860763db77d0bc477da28302806d4a4bbc60db
Sean Roades <seanroades@college.harvard.edu>
2023-01-28 03:57:44 -0500

    Finished

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,191 @@
 <script lang="ts">
-	export let name: string;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+	let todos: string[] = [];
+	let task: string = "";
+	let filter: string[] = "all";
+	function addTask() { // ReactiveDeclaration, Life Cycle
+		if (task.length > 50) {
+			alert("Your task cannot be greater than 50 characters long"); // Printing
+			task = ""
+			return;
+		}
+		if (task.length >= 2) {
+			todos = [{
+				task: task,
+				status: 'pending'
+			}, ...todos];
+			task = "";
+		}
+		else {
+			alert('Your task must be at least 2 characters long') // Printing
 		}
 	}
+	function markComplete(i) { // ReactiveDeclaration, Life Cycle
+		todos[i].status = "completed";
+		todos = [...todos];
+	}
+	function removeTask(i) { // ReactiveDeclaration, Life Cycle
+		todos.splice(i, 1);
+		todos = [...todos];
+	}
+</script>
+
+<style>
+	* {
+		margin: 0px;
+		padding: 0px
+	}
+	.container {
+		width: 100%;
+		height: 100vh;
+		background: white;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+	.todo {
+		padding: 20px;
+		background: #f5f5f5;
+		width: 450px;
+		border-radius: 20px;
+	}
+	.todo > div {
+		margin: 20px 0px;
+	}
+	.todo .form{
+		display: flex;
+	}
+	.todo .form input,
+	.todo .form button {
+		border: 1px solid #4db848;
+		height: 40px;
+		outline: none;
+		border-radius: 20px;
+	}
+	.todo .form input {
+		flex: 1;
+		text-indent: 20px;
+	}
+	.todo .form button {
+		width: 60px;
+		margin-left: 5px;
+		color: #4db848;
+		cursor: pointer;
+	}
+	.todo .tasks {
+		min-height:100px;
+	}
+	.todo .tasks > .task {
+		margin: 10px 0px;
+		display: flex;
+	}
+	.todo .tasks > .task > div {
+		flex:1;
+	}
+	.todo .tasks > .task > button {
+		width: 25px;
+		height: 25px;
+		border: 1px solid #4db848;
+		color: #4db848;
+		border-radius: 50%;
+		margin: 0px 5px;
+		cursor: pointer;
+	}
+	.todo .tasks > .task > button.active{
+		background: #4db848;
+		color: white;
+	}
+	.todo .filters {
+		display: flex;
+		justify-content: space-between;
+	}
+	.todo .filters > button {
+		padding:10px 8px;
+		border:1px solid #4db848;
+		color: #4db848;
+		border-radius: 20px;
+		cursor: pointer;
+		min-width: 100px;
+	}
+	.todo .filters > button.active {
+		background: #4db848;
+		color: white;
+		font-weight: bold;
+	}
+	.title {
+		color: #4db848;
+		font-size: 36px;
+		margin-bottom: 10px;
+		font-weight: bold;
+	}
+	.subtitle {
+		color: #666666;
+		font-size: 12px;
+		margin-bottom: 10px;
+		font-weight: bold;
+	}
+	.button {
+		min-width: 100px;
+	}
 </style>
+
+<div class="container">
+	<p class="title">CS178 Todo</p> <!--Headings-->
+	<p class="subtitle">By Sean Roades</p>
+	<div class="todo">
+		<div class = "form">
+			<input type="text" bind:value={task} /> <!--Properties--> <!--Binding-->
+			<button class="button" on:click={addTask}>Add Task</button> <!--Properties-->
+		</div>
+		<div class="tasks">
+			{#each todos as todo, i} <!--controlFlow--> <!--template-->
+				{#if filter === 'all'} <!--controlFlow-->
+					<div class="task">
+						<div>{todo.task}</div> <!--format--> <!--stateful-->
+						<button 
+							class="{(todo.status === 'completed') ? 'active' : ''}" 
+							on:click={() => {markComplete(i)}}> <!--Attributes-->
+							&#10004;
+						</button> <!--controlFlow--> <!--Properties--> <!--Button-->
+						<button on:click={() => {removeTask(i)}}>&#10006</button> <!--Attributes--> <!--Button-->
+					</div>
+				{:else if filter === 'completed'} <!--controlFlow--> <!--template-->
+					{#if todo.status === 'completed'} <!--controlFlow-->
+						<div class="task"> 
+							<div>{todo.task}</div> <!--format--> <!--stateful-->
+							<button on:click={() => removeTask(i)}> <!--Attributes-->
+								&#10006
+							</button> <!--controlFlow--> <!--Button-->
+						</div>
+					{/if} <!--controlFlow-->
+				{:else} <!--controlFlow--> <!--template-->
+					{#if todo.status === 'pending'} <!--controlFlow-->
+						<div class="task">
+							<div>{todo.task}</div> <!--format--> <!--stateful-->
+							<button 
+								class="{todo.status=='completed' ? 'active' : ''}"
+								on:click={() => (markComplete(i))}> <!--Attributes-->
+							&#10004
+							</button> <!--controlFlow--> <!--Properties--> <!--Button-->
+						</div>
+					{/if} <!--controlFlow-->
+				{/if} <!--controlFlow-->
+			{/each} <!--controlFlow-->
+		</div>
+		<div class="filters">
+			<button 
+				class="{filter === 'all' ? 'active' : 'button'}" 
+				on:click={() => {filter = 'all'}}>
+				All
+			</button> <!--controlFlow--> <!--Properties--> <!--Button-->
+			<button 
+				class="{filter === 'completed' ? 'active' : 'button'}" 
+				on:click={() => {filter = 'completed'}}> 
+				Completed
+			</button> <!--controlFlow--> <!--Properties--> <!--Button-->
+			<button class="{filter === 'incomplete' ? 'active' : 'button'}" on:click={() => {filter = 'incomplete'}}>
+				Incomplete
+			</button> <!--controlFlow--> <!--Properties--> <!--Button-->
+		</div>
+	</div>
+</div>
